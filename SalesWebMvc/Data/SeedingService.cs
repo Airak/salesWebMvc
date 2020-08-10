68ca389213ce642efe70ee6129a7a6d588923c12
Matheus Martins <matheus.mmag@gmail.com>
2020-08-10 18:43:39 -0300

    Seeding Service

---
+++ SalesWebMvc/Data/SeedingService.cs
@@ -0,0 +1,41 @@
+﻿using SalesWebMvc.Models;
+using SalesWebMvc.Models.Enums;
+using SalesWebMvc.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Data
+{
+    public class SeedingService
+    {
+        private SalesWebMvcContext _context;
+
+        public SeedingService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Department.Any() || _context.Seller.Any() || _context.SalesRecord.Any())
+            {
+                return;
+            }
+
+            Department d1 = new Department(1, "Computers");
+            Department d2 = new Department(2, "Electronics");
+
+            Seller s1 = new Seller(1, "Juan", "juan@gmail.com", new DateTime(1998, 4, 21), 1000.0, d1);
+
+            SalesRecord sr1 = new SalesRecord(1, new DateTime(2018, 09, 25), 11000.0, SaleStatus.Billed, s1);
+
+            _context.Department.AddRange(d1, d2);
+            _context.Seller.Add(s1);
+            _context.SalesRecord.Add(sr1);
+
+            _context.SaveChanges();
+        }
+    }
+}
